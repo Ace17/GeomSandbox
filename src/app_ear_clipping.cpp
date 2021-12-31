fb3a74ef75d4f0d358210e50608bd2b42f84d0e2
Vivien <bonnet.vivien@gmail.com>
2021-12-31 16:59:03 +0100

    ear-clipping: implementation.

--- src/app_ear_clipping.cpp
+++ src/app_ear_clipping.cpp
@@ -7,6 +7,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Polygon Triangulation: Ear-clipping Algorithm.
 
+#include <algorithm>
 #include <cmath>
 #include <vector>
 
@@ -16,13 +17,104 @@
 
 namespace
 {
-struct EarClippingAlgorithm
+static float cross(Vec2 v, Vec2 w) { return v.x * w.y - v.y * w.x; }
+
+struct Segment
 {
-  struct Segment
+  int a, b;
+};
+
+struct Ear
+{
+  int tip;
+  int a, b;
+};
+
+void drawPolygon(IDrawer* drawer, const Polygon2f& input, Color color)
+{
+  for(Face face : input.faces)
+    drawer->line(input.vertices[face.a], input.vertices[face.b], color);
+}
+
+Ear polygonEarFromIndex(const Polygon2f& polygon, int index)
+{
+  Ear ear;
+  ear.tip = index;
+  ear.a = -1;
+  ear.b = -1;
+  for(Face face : polygon.faces)
   {
-    int a, b;
+    if(face.a == index)
+      ear.b = face.b;
+    else if(face.b == index)
+      ear.a = face.a;
+  }
+
+  return ear;
+}
+
+bool isInsideTriangle(Vec2 a, Vec2 b, Vec2 c, Vec2 point)
+{
+  auto isOnTheRightOfSegment = [](Vec2 segmentStart, Vec2 segmentEnd, Vec2 point) {
+    const Vec2 a = point - segmentStart;
+    const Vec2 b = segmentEnd - point;
+    const float epsilon = 0.0001;
+    return cross(a, b) > -epsilon;
   };
 
+  return isOnTheRightOfSegment(a, b, point) && isOnTheRightOfSegment(b, c, point) && isOnTheRightOfSegment(c, a, point);
+}
+
+bool isValidEar(const Polygon2f& polygon, Ear& ear)
+{
+  if(ear.a == -1 || ear.b == -1)
+    return false;
+
+  const Vec2 a = polygon.vertices[ear.a];
+  const Vec2 b = polygon.vertices[ear.b];
+  const Vec2 tip = polygon.vertices[ear.tip];
+  const Vec2 segmentA = tip - a;
+  const Vec2 segmentB = b - tip;
+  const float epsilon = 0.0001; // In order to ignore "flat" corners.
+  const bool convexAngle = cross(segmentA, segmentB) < -epsilon;
+  if(!convexAngle)
+    return false;
+
+  for(int idx = 0; idx < polygon.vertices.size(); idx++)
+  {
+    if(idx != ear.tip && idx != ear.a && idx != ear.b && isInsideTriangle(a, tip, b, polygon.vertices[idx]))
+      return false;
+  }
+
+  return true;
+};
+
+void removeFacesForCorner(Polygon2f& polygon, int idx)
+{
+  auto isConcernedFace = [idx](Face face) { return (face.a == idx || face.b == idx); };
+  auto it = std::remove_if(polygon.faces.begin(), polygon.faces.end(), isConcernedFace);
+  polygon.faces.erase(it, polygon.faces.end());
+}
+
+Segment clipEar(Polygon2f& polygon)
+{
+  for(int idx = 0; idx < polygon.vertices.size(); idx++)
+  {
+    Ear ear = polygonEarFromIndex(polygon, idx);
+    if(isValidEar(polygon, ear))
+    {
+      removeFacesForCorner(polygon, idx);
+      polygon.faces.push_back({ear.a, ear.b});
+      return {ear.a, ear.b};
+    }
+  }
+
+  // Should never be reached : a polygon with 4 or more vertices should have at least 2 ears.
+  return {};
+}
+
+struct EarClippingAlgorithm
+{
   static Polygon2f generateInput()
   {
     IVisualizer* visualizerForPolygon = nullptr;
@@ -48,19 +140,25 @@ struct EarClippingAlgorithm
 
   static std::vector<Segment> execute(Polygon2f input)
   {
-    // TODO
-    return {};
+    std::vector<Segment> result;
+    while(input.faces.size() > 3)
+    {
+      drawPolygon(gVisualizer, input, Yellow);
+      gVisualizer->step();
+      Segment segment = clipEar(input);
+      result.push_back(segment);
+    }
+    drawPolygon(gVisualizer, input, Yellow);
+    gVisualizer->step();
+    return result;
   }
 
-  static void drawInput(IDrawer* drawer, const Polygon2f& input)
-  {
-    for(Face face : input.faces)
-      drawer->line(input.vertices[face.a], input.vertices[face.b]);
-  }
+  static void drawInput(IDrawer* drawer, const Polygon2f& input) { drawPolygon(drawer, input, White); }
 
   static void drawOutput(IDrawer* drawer, const Polygon2f& input, const std::vector<Segment>& output)
   {
-    // TODO
+    for(auto& segment : output)
+      drawer->line(input.vertices[segment.a], input.vertices[segment.b], Green);
   }
 };
 
