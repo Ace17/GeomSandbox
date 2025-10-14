2769a00745c872c2831abd51ce60db08ff1ad950
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2025-10-14 08:44:14 +0200

    Fix segment intersection function

--- src/app_segments_intersect.cpp
+++ src/app_segments_intersect.cpp
@@ -12,91 +12,125 @@
 #include "core/geom.h"
 
 #include <cmath>
+#include <iterator>
+#include <vector>
 
 #include "random.h"
 
 namespace
 {
-float sqr(float v) { return v * v; }
 
-float sqrMagnitude(Vec2 a) { return dotProduct(a, a); }
+float det2d(Vec2 a, Vec2 b) { return a.x * b.y - a.y * b.x; }
 
-bool segmentsIntersect(Vec2 u0, Vec2 u1, Vec2 v0, Vec2 v1, float toleranceRadius, Vec2& where)
+bool compare(Vec2 a, Vec2 b)
 {
-  // raycast of u0->u1 against the "wall" v0v1
-  const Vec2 tangent = normalize(v1 - v0);
-  const Vec2 normal = rotateLeft(tangent);
+  if(a.x != b.x)
+    return a.x < b.x;
+  if(a.y != b.y)
+    return a.y < b.y;
+  return false;
+}
 
-  // project everybody on this normal
-  const float pos0 = dotProduct(normal, u0);
-  const float pos1 = dotProduct(normal, u1);
-  const float posWall = dotProduct(normal, v0);
+bool onSegment(Vec2 u0, Vec2 u1, Vec2 p)
+{
+  // be invariant to the order of u0 and u1
+  if(compare(u0, u1))
+    std::swap(u0, u1);
 
-  if(pos0 > posWall + toleranceRadius && pos1 > posWall + toleranceRadius)
+  if(dotProduct(p - u0, p - u1) > 0)
     return false;
+  return det2d(p - u0, u1 - u0) == 0;
+}
+}
 
-  if(pos0 < posWall - toleranceRadius && pos1 < posWall - toleranceRadius)
-    return false;
-
-  float fraction;
-
-  // parallel segments
-  if(fabs(pos1 - pos0) < toleranceRadius)
+bool segmentsIntersect(Vec2 u0, Vec2 u1, Vec2 v0, Vec2 v1, Vec2& where)
+{
+  // First, check for intersections on endpoints
+  // (This also handles the case where the segments are parallel)
+  // Beware, by convention:
+  // - if both u0 and u1 are in contact with v, we return u0.
+  // - if both v0 and v1 are in contact with u, we return v0.
   {
-    if(fabs(pos0 - posWall) > toleranceRadius)
-      return false; // not colinear enough
-
-    const float uMin = std::min(dotProduct(tangent, u0), dotProduct(tangent, u1));
-    const float uMax = std::max(dotProduct(tangent, u0), dotProduct(tangent, u1));
-
-    const float vMin = std::min(dotProduct(tangent, v0), dotProduct(tangent, v1));
-    const float vMax = std::max(dotProduct(tangent, v0), dotProduct(tangent, v1));
-
-    if(uMax < vMin - toleranceRadius || uMin > vMax + toleranceRadius)
-      return false; // no common point
-
-    // now, there are 2 possibilites:
-    // 1) uMin is less than vMin-toleranceRadius
-    // 2) uMin is between vMin-toleranceRadius and vMax+toleranceRadius
-    if(uMin < vMin - toleranceRadius)
+    if(onSegment(u0, u1, v0))
     {
-      fraction = (vMin - uMin) / (uMax - uMin); // common point at vMin
+      where = v0;
+      return true;
     }
-    else
+    if(onSegment(v0, v1, u0))
     {
-      fraction = 0; // uMin is between vMin and vMax.
-                    // choose uMin as our common point.
+      where = u0;
+      return true;
+    }
+    if(onSegment(u0, u1, v1))
+    {
+      where = v1;
+      return true;
+    }
+    if(onSegment(v0, v1, u1))
+    {
+      where = u1;
+      return true;
     }
   }
-  else
+
+  // Be invariant to the order of the input points
   {
-    fraction = (posWall - pos0) / (pos1 - pos0);
+    if(compare(u0, u1))
+      std::swap(u0, u1);
+
+    if(compare(v0, v1))
+      std::swap(v0, v1);
   }
 
-  if(fraction < -toleranceRadius || fraction > 1 + toleranceRadius)
-    return false;
+  // Determine if the infinite lines (u0,u1) and (v0,v1) intersect.
+  {
+    const int side_v0 = det2d(u1 - u0, v0 - u0) > 0 ? 1 : -1;
+    const int side_v1 = det2d(u1 - u0, v1 - u0) > 0 ? 1 : -1;
+    if(side_v0 == side_v1)
+      return false; // the lines don't cross
+  }
 
-  // intersection of u0u1 with the line v0v1
-  where = u0 + fraction * (u1 - u0);
+  // The infinite lines (u0,u1) and (v0,v1) intersect. Find where.
+  const auto normal = rotateLeft(v1 - v0);
 
-  // check if the intersection belongs to the segment v0v1
-  if(dotProduct(where - v0, tangent) < -toleranceRadius)
-    return false;
+  const float pos_u0 = dotProduct(normal, u0);
+  const float pos_u1 = dotProduct(normal, u1);
+  const float pos_wall = dotProduct(normal, v0);
 
-  if(dotProduct(where - v1, tangent) > +toleranceRadius)
-    return false;
+  const float fraction = (pos_wall - pos_u0) / (pos_u1 - pos_u0);
+  where = u0 + (u1 - u0) * fraction;
 
-  // we exclude the circle centered on u1, with a radius of toleranceRadius.
-  if(sqrMagnitude(where - u1) < sqr(toleranceRadius))
-    return false;
-
-  // we exclude the circle centered on v1, with a radius of toleranceRadius.
-  if(sqrMagnitude(where - v1) < sqr(toleranceRadius))
-    return false;
+  if(fraction > 1 || fraction < 0)
+    return false; // the intersection is outside [u0,u1]
 
   return true;
 }
 
+struct Crossing
+{
+  int i;
+  Vec2 where;
+};
+
+std::vector<Crossing> computeSegmentVsPolylineIntersections(Vec2 u0, Vec2 u1, span<const Vec2> polyline)
+{
+  std::vector<Crossing> r;
+
+  for(int i = 0; i + 1 < (int)polyline.len; ++i)
+  {
+    Vec2 where;
+    if(segmentsIntersect(u0, u1, polyline[i], polyline[i + 1], where))
+    {
+      if(!(where == polyline[i + 1]))
+        r.push_back({i, where});
+    }
+  }
+
+  return r;
+}
+
+namespace
+{
 Vec2 randomPosition()
 {
   Vec2 r;
@@ -109,29 +143,48 @@ struct SegmentIntersectionApp : IApp
 {
   SegmentIntersectionApp()
   {
+    points.resize(10);
+
     for(auto& p : points)
       p = randomPosition();
 
-    points[0] = {-5, 0.2};
-    points[1] = {+5, 0.2};
-    points[2] = {-10, 0};
-    points[3] = {+10, 0};
+    {
+      points[0] = {-12.5, 0};
+      points[1] = {+10, 0};
+
+      points[2] = {0, -10};
+      points[3] = {0, 0};
+      points[4] = {0, 10};
+    }
+
+    {
+      points[0] = {0, -5};
+      points[1] = {0, +5};
+
+      points[2] = {0, -10};
+      points[3] = {0, +10};
+      points[4] = {10, +10};
+    }
 
     compute();
   }
 
   void draw(IDrawer* drawer) override
   {
-    drawer->line(points[0], points[1], m_doIntersect ? Red : White);
-    drawer->line(points[2], points[3], m_doIntersect ? Red : White);
+    drawer->line(points[0], points[1], Yellow);
+
+    for(int i = 2; i + 1 < (int)std::size(points); ++i)
+      drawer->line(points[i], points[i + 1], White);
 
     for(auto p : points)
-      drawer->circle(p, m_toleranceRadius, White);
+      drawer->rect(p, {}, White, {8, 8});
 
-    if(m_doIntersect)
+    for(auto intersection : m_intersections)
     {
-      drawer->line(m_where - Vec2(0.4, 0), m_where + Vec2(0.4, 0), Green);
-      drawer->line(m_where - Vec2(0, 0.4), m_where + Vec2(0, 0.4), Green);
+      drawer->line(intersection.where - Vec2(0.4, 0), intersection.where + Vec2(0.4, 0), Green);
+      drawer->line(intersection.where - Vec2(0, 0.4), intersection.where + Vec2(0, 0.4), Green);
+
+      drawer->line(points[2 + intersection.i], points[2 + intersection.i + 1], Red);
     }
 
     // draw selected point
@@ -146,7 +199,7 @@ struct SegmentIntersectionApp : IApp
 
   void keydown(Key key)
   {
-    const auto speed = 0.1;
+    const auto speed = 0.125;
 
     switch(key)
     {
@@ -162,14 +215,11 @@ struct SegmentIntersectionApp : IApp
     case Key::Down:
       points[m_cur].y -= speed;
       break;
-    case Key::PageUp:
-      m_toleranceRadius *= 1.01;
-      break;
-    case Key::PageDown:
-      m_toleranceRadius *= 1.0 / 1.01;
+    case Key::Return:
+      std::swap(points[m_cur / 2], points[m_cur / 2 + 1]);
       break;
     case Key::Space:
-      m_cur = (m_cur + 1) % 4;
+      m_cur = (m_cur + 1) % std::size(points);
       break;
     default:
       break;
@@ -180,15 +230,15 @@ struct SegmentIntersectionApp : IApp
 
   void compute()
   {
-    m_doIntersect = segmentsIntersect(points[0], points[1], points[2], points[3], m_toleranceRadius, m_where);
+    span<const Vec2> polyline(std::size(points) - 2, points.data() + 2);
+    m_intersections = computeSegmentVsPolylineIntersections(points[0], points[1], polyline);
   }
 
   int m_cur = 0;
-  Vec2 points[4];
-  Vec2 m_where{};
-  float m_toleranceRadius = 0.5;
-  bool m_doIntersect = false;
+  std::vector<Vec2> points;
+  std::vector<Crossing> m_intersections;
 };
 
-const int registered = registerApp("Intersection/Segments", []() -> IApp* { return new SegmentIntersectionApp; });
+const int registered =
+      registerApp("Intersection/SegmentVsPolyline", []() -> IApp* { return new SegmentIntersectionApp; });
 }
