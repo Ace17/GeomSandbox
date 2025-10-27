7ca4d11aa63a3571079bf40ce6cdb5d09585fef7
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2025-10-27 21:01:28 +0100

    Random/Polyhedron: port to new API

--- src/app_random_polyhedron.cpp
+++ src/app_random_polyhedron.cpp
@@ -7,7 +7,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Random polyhedron generation
 
-#include "core/algorithm_app.h"
+#include "core/algorithm_app2.h"
 #include "core/sandbox.h"
 
 #include "random_polyhedron.h"
@@ -15,46 +15,45 @@
 namespace
 {
 
-struct RandomPolygon
+int generateInput(int /*seed*/) { return 0; }
+
+PolyhedronFL execute(int /*unused*/) { return createRandomPolyhedronFL(); }
+
+void display(int /*input*/, const PolyhedronFL& output)
 {
-  static int generateInput() { return 0; }
+  sandbox_line({0, 0, 0}, {1, 0, 0}, Red);
+  sandbox_line({0, 0, 0}, {0, 1, 0}, Green);
+  sandbox_line({0, 0, 0}, {0, 0, 1}, LightBlue);
 
-  static PolyhedronFL execute(int /*seed*/) { return createRandomPolyhedronFL(); }
-
-  static void display(int /*input*/, const PolyhedronFL& output)
+  for(auto& face : output.faces)
   {
-    sandbox_line({0, 0, 0}, {1, 0, 0}, Red);
-    sandbox_line({0, 0, 0}, {0, 1, 0}, Green);
-    sandbox_line({0, 0, 0}, {0, 0, 1}, LightBlue);
+    const int N = face.indices.size();
 
-    for(auto& face : output.faces)
+    Vec3 faceCenter{};
+    for(int i = 0; i < N; ++i)
     {
-      const int N = face.indices.size();
+      auto a = output.vertices[face.indices[i]];
+      auto b = output.vertices[face.indices[(i + 1) % N]];
+      sandbox_line(a, b, Yellow);
+      faceCenter += a;
+    }
 
-      Vec3 faceCenter{};
-      for(int i = 0; i < N; ++i)
-      {
-        auto a = output.vertices[face.indices[i]];
-        auto b = output.vertices[face.indices[(i + 1) % N]];
-        sandbox_line(a, b, Yellow);
-        faceCenter += a;
-      }
+    {
+      faceCenter *= (1.0f / N);
 
-      {
-        faceCenter *= (1.0f / N);
+      const auto A = output.vertices[face.indices[0]];
+      const auto B = output.vertices[face.indices[1]];
+      const auto C = output.vertices[face.indices[2]];
+      Vec3 faceNormal = normalize(crossProduct(B - A, C - A));
 
-        const auto A = output.vertices[face.indices[0]];
-        const auto B = output.vertices[face.indices[1]];
-        const auto C = output.vertices[face.indices[2]];
-        Vec3 faceNormal = normalize(crossProduct(B - A, C - A));
-
-        sandbox_line(faceCenter + faceNormal * 0.5, faceCenter + faceNormal * 1.0f, {0.4f, 0, 0, 1});
-        sandbox_line(faceCenter, faceCenter + faceNormal * 0.5f, {0.7f, 0, 0, 1});
-      }
+      sandbox_line(faceCenter + faceNormal * 0.5, faceCenter + faceNormal * 1.0f, {0.4f, 0, 0, 1});
+      sandbox_line(faceCenter, faceCenter + faceNormal * 0.5f, {0.7f, 0, 0, 1});
     }
   }
-};
+}
 
-IApp* create() { return createAlgorithmApp(std::make_unique<ConcreteAlgorithm<RandomPolygon>>()); }
-const int reg = registerApp("Random/Polyhedron", &create);
+BEGIN_ALGO("Random/Polyhedron", execute)
+WITH_INPUTGEN(generateInput)
+WITH_DISPLAY(display)
+END_ALGO
 }
